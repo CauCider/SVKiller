49c3cdce444e202b9990e3adbe01f58c7f3670da
hyunju <dlguswn7768@gmail.com>
2021-10-10 21:29:59 +0900

    impl : 1인칭 single fps 학교맵 구현 완료

--- Respawn_Point_Setter.gd
+++
@@ -1,7 +0,0 @@
-extends Spatial
-
-func _ready():
-	# Get the globals autoload script
-	var globals = get_node("/root/Globals")
-	# We assume every child is a respawn point, so we add all of our children as the spawn point
-	globals.respawn_points = get_children()
